70277edeca1a609a89b3fa8758ae33da5389760a
jjk235 <jaejunkwak@outlook.com>
2018-08-18 19:47:24 -0700

    Update assignment_3.py

--- football_dictionaries/assignment_3.py
+++ football_dictionaries/assignment_3.py
@@ -1,2 +1,12 @@
-def players_by_country_and_position(squads_list):
-    pass
+
+def players_by_country_and_position(SQUADS_DATA):
+    new_country_dict={}
+    for key,pos_dict in SQUADS_DATA.items():
+     
+        for pos_player in pos_dict:   
+            new_country_dict.setdefault(pos_player['country'],{})
+            new_country_dict[pos_player['country']].setdefault(pos_player['position'],[])
+            
+            new_country_dict[pos_player['country']][pos_player['position']].append(pos_player)
+            
+    return new_country_dict
