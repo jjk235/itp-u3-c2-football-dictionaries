627be591b9e5727a5aa3e29a354eaeed4e3edc40
jjk235 <jaejunkwak@outlook.com>
2018-08-18 17:47:57 -0700

    Update assignment_3.py

--- warm_up/assignment_3.py
+++ warm_up/assignment_3.py
@@ -1,2 +1,10 @@
 def transform_multiple_users_to_dicts(multiple_users):
-    pass
+    users_as_dict=[]
+    for i in multiple_users:
+        new_dict={
+            'name':i[0
+            'email':i[1]
+            'age':i[2]
+        }
+        users_as_dict.append(new_dict)
+    return users_as_dict
