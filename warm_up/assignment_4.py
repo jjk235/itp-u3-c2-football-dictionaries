63a7f12c66418b18fe518b3a37db666988123e92
jjk235 <jaejunkwak@outlook.com>
2018-08-18 18:22:35 -0700

    Update assignment_4.py

--- warm_up/assignment_4.py
+++ warm_up/assignment_4.py
@@ -1,2 +1,7 @@
 def group_users_by_email_domain(users):
-    pass
+    new_dict={}
+    for each_user in users:
+        email_key=each_user['email'][each_user['email'].index("@")+1:len(each_user['email'])]
+        new_dict.setdefault(email_key,[])
+        new_dict[email_key].append(each_user)
+    return new_dict
